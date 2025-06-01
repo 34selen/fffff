5daed8882606d1800f037936b398cfb35e8705b4
34selen <73600142+34selen@users.noreply.github.com>
2025-06-01 19:19:14 +0900

    f

--- deploy/app/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export FLAG=$(cat ./flag.txt)
-
-exec "$@"
--- deploy/app/utils/balance.js
+++
@@ -1,6 +0,0 @@
-let balance = 1;
-
-const getBalance = () => balance;
-const setBalance = (newBalance) => { balance = newBalance; };
-
-module.exports = { getBalance, setBalance };
